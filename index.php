e8576f3bb831e8f39eecf1b8c7bda2c3b6d9c611
Dissenter12 <maverickpunzalan@yahoo.com>
2017-06-13 16:52:28 +0800

    version1.1

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<br>my true love sent to me:</h1><br><span id="gift"></span><br>
 	<button id='start' >Start</button>
 	<hr>
-	<h1>On the <span id="day1"></span> day of Christmas
+	<h1 id="wala">On the <span id="day1"></span> day of Christmas
 		<br>my true love sent to me:</h1><br><span id="gift1"></span><br>
 	<button id='button' >Start</button>
 
@@ -27,8 +27,6 @@
 					'11 Pipers Piping<br>10 Lords a Leaping<br>9 Ladies Dancing<br>8 Maids a Milking<br>7 Swans a Swimming<br>6 Geese a Laying<br>5 Golden Rings<br>4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree'  => '11th',
 					'12 Drummers Drumming<br>11 Pipers Piping<br>10 Lords a Leaping<br>9 Ladies Dancing<br>8 Maids a Milking<br>7 Swans a Swimming<br>6 Geese a Laying<br>5 Golden Rings<br>4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree' => '12th');
 
-	$days = array('first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth');
-	$gave = array('A Partridge in a Pear Tree', '2 Turtle Doves', '3 French Hens', '4 Calling Birds', '5 Golden Rings', '6 Geese a Laying', '7 Swans a Swimming', '8 Maids a Milking', '9 Ladies Dancing', '10 Lords a Leaping', '11 Pipers Piping', '12 Drummers Drumming');
 
 	?>
 
@@ -49,15 +47,30 @@
 	}
 
 	var days = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth'];
-	var gave = ['A Partridge in a Pear Tree', '2 Turtle Doves', '3 French Hens', '4 Calling Birds', '5 Golden Rings', '6 Geese a Laying', '7 Swans a Swimming', '8 Maids a Milking', '9 Ladies Dancing', '10 Lords a Leaping', '11 Pipers Piping', '12 Drummers Drumming'];
+	var gave = ['A Partridge in a Pear Tree', 
+	'Two Turtle Doves<br>and a Partridge in a Pear Tree.', 
+	'Three French Hens,<br>Two Turtle Doves<br>and a Partridge in a Pear Tree.', 
+	'4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree', 
+	'5 Golden Rings<br>4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree', 
+	'6 Geese a Laying<br>5 Golden Rings<br>4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree', 
+	'7 Swans a Swimming<br>6 Geese a Laying<br>5 Golden Rings<br>4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree', 
+	'8 Maids a Milking<br>7 Swans a Swimming<br>6 Geese a Laying<br>5 Golden Rings<br>4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree', 
+	'9 Ladies Dancing<br>8 Maids a Milking<br>7 Swans a Swimming<br>6 Geese a Laying<br>5 Golden Rings<br>4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree', 
+	'10 Lords a Leaping<br>9 Ladies Dancing<br>8 Maids a Milking<br>7 Swans a Swimming<br>6 Geese a Laying<br>5 Golden Rings<br>4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree', 
+	'11 Pipers Piping<br>10 Lords a Leaping<br>9 Ladies Dancing<br>8 Maids a Milking<br>7 Swans a Swimming<br>6 Geese a Laying<br>5 Golden Rings<br>4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree', 
+	'12 Drummers Drumming<br>11 Pipers Piping<br>10 Lords a Leaping<br>9 Ladies Dancing<br>8 Maids a Milking<br>7 Swans a Swimming<br>6 Geese a Laying<br>5 Golden Rings<br>4 Calling Birds<br>3 French Hens<br>2 Turtle Doves<br>and a Partridge in a Pear Tree'];
 
 	var x=0;
 	document.getElementById('button').onclick = function(){
 		document.getElementById('button').innerHTML= 'Next';
-		gave[x] += gave[x];
+
 				document.getElementById('day1').innerHTML= days[x];
 				document.getElementById('gift1').innerHTML= gave[x];
 	x++;
+		if (x>=13) {
+			document.getElementById('wala').innerHTML = "binawi lahat";
+			document.getElementById('gift1').innerHTML= "";
+		}
 	}
 </script>
 
